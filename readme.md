480640de18bc3fcb8d7ff85d0740f630e2d42d43
Oliver Blanthorn <freedom4cows@gmail.com>
2024-09-14 17:24:51 +0200

    Add tiles and tidy up

--- readme.md
+++ readme.md
@@ -1,18 +1,31 @@
-# H3-MON: THE MOST POWERFUL MON(itor) IN THE UNIVERSE
+# French IRIS tiled map
 
-A simple data vis tool using MapLibre GL and deck.gl to display and refresh data from a CSV file.
+A simple data vis tool using MapLibre GL and deck.gl to display data from a CSV file joined with the French INSEE communes
 
 <p align="center">
-<img src="promo/demo.png" alt="An astonishingly beautiful map of the UK">
+<img src="promo/demo.png" alt="Chloropleth map of Paris showing percentage of principal residences without cars">
 </p>
-
 
 # How to run
 
-Prerequisites: yarn. A web browser. A CSV file of index, value for [H3 Hexagon indices](https://h3geo.org/).
+Prerequisites: yarn. A web browser. A CSV file of IRIS code, value.
 
 0. `git clone`
 1. `yarn install`
-2. bung data in `./www/data/h3_data.csv` with index (hex strings), values normalised from 0-1
+2. bung data in `./www/data/iris_data.csv` with numeric IRIS code, values normalised from 0-1
 3. `yarn serve&; yarn watch`, open localhost:1983
-4. update data however you like, it gets refreshed every few seconds
+4. reload page to reload data
+
+(nb: at the moment the value column is called perc_voit and the tooltip hardcodes it as percent_zero_voitures)
+
+
+# Dealing with France
+
+convert lambert 93 (or other) shapefile with wgs84 geojson then make tiles
+```
+ogr2ogr -f GeoJSON -t_srs EPSG:4326 iris_2020.geojson CONTOURS-IRIS.shp 
+tippecanoe -zg --coalesce-smallest-as-needed --no-tile-compression -e tiles iris_2020.geojson # pay attention to min/max zoom in tiles directory and set to right value in deck.gl
+```
+
+# Copyright
+IRIS tiles copyright INSEE and IGN
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import {MapboxOverlay} from '@deck.gl/mapbox'
-import {H3HexagonLayer} from '@deck.gl/geo-layers'
+import {MVTLayer} from '@deck.gl/geo-layers'
 import {CSVLoader} from '@loaders.gl/csv'
+import {load} from '@loaders.gl/core'
 import maplibregl from 'maplibre-gl'
 import * as d3 from 'd3'
 import 'maplibre-gl/dist/maplibre-gl.css'
@@ -20,20 +21,21 @@ const colourRamp = d3.scaleSequential(d3.interpolateSpectral).domain([0,1])
 
 /* convert from "rgba(r,g,b,a)" string to [r,g,b] */
 const getColour = v => Object.values(d3.color(colourRamp(v))).slice(0,-1)
-// const getColour = v => [...Object.values(d3.color(colourRamp(v))).slice(0,-1), Math.sqrt(v)*255] // with v as alpha too
 let reloadNum = 0
-const getHexData = () => new H3HexagonLayer({
-    id: 'H3HexagonLayer',
-    data: `/data/h3_data.csv?v=${++reloadNum}`,
-    loaders: [CSVLoader],
+const getIrisData = csvmap => {
+    return new MVTLayer({
+    id: 'IrisLayer',
+    minZoom: 0,
+    maxZoom: 9,
+    data: '/data/shapefiles/CONTOURS-IRIS_2-1_SHP_LAMB93_FXX-2020/tiles/{z}/{x}/{y}.pbf',
     extruded: false,
-    stroked: false,
-    getHexagon: d => d.index,
-    getFillColor: d => getColour(d.value),
-    getElevation: d => (1-d.value)*1000,
-    elevationScale: 20,
+    stroked: true,
+    getFillColor: d => {
+        const v = csvmap.get(parseInt(d.properties.CODE_IRIS))
+        return v == undefined ? [255, 255, 255, 0] : getColour(v)
+    },
     pickable: true
-})
+})}
 
 function getTooltip({object}) {
     const toDivs = kv => {
@@ -41,7 +43,7 @@ function getTooltip({object}) {
     }
     return object && {
         // html: `<div>${(object.value).toPrecision(2)}</div>`,
-        html: Object.entries(object).map(toDivs).join(" "),
+        html: Object.entries({...object.properties, percent_zero_voitures: csvmap.get(parseInt(object.properties.CODE_IRIS))}).map(toDivs).join(" "),
         style: {
             backgroundColor: '#fff',
             fontFamily: 'sans-serif',
@@ -54,28 +56,32 @@ function getTooltip({object}) {
 
 const mapOverlay = new MapboxOverlay({
     interleaved: false,
-    // onClick: (info, event) => {
-    //     if (info.layer && info.layer.id === 'H3HexagonLayer') {
-    //         console.log('Clicked H3 index:', info.object.index);
-    //     }
-    // },
+    onClick: (info, event) => {
+        if (info.layer) {
+            console.log(info.object);
+        }
+    },
     getTooltip,
 })
 
 map.addControl(mapOverlay)
 map.addControl(new maplibregl.NavigationControl())
 
-const update = () => {
-    mapOverlay.setProps({layers:[getHexData()]})
+let csvmap = new Map()
+const update = async () => {
+    const csvdata = (await load("/data/iris_data.csv", CSVLoader)).data
+    csvmap = new Map(csvdata.map(r => [r.IRIS, r.perc_voit]))
+    mapOverlay.setProps({layers:[getIrisData(csvmap)]})
 }
 update()
+
 
 window.d3 = d3
 window.observablehq = observablehq
 
 const params = new URLSearchParams(window.location.search)
 const l = document.getElementById("attribution")
-l.innerText = "© " + [params.get('c'), "MapTiler",  "OpenStreetMap contributors"].filter(x=>x !== null).join(" © ")
+l.innerText = "© " + ["INSEE", "MapTiler",  "OpenStreetMap contributors"].filter(x=>x !== null).join(" © ")
 // todo: read impressum from metadata too
 async function makeLegend() {
     try {
@@ -88,37 +94,8 @@ async function makeLegend() {
     }
 }
 makeLegend()
-
-
-try {
-    const socket = new WebSocket("ws://localhost:1990")
-    socket.addEventListener("open", (event) => {
-        socket.send("ping")
-    })
-    // Update whenever you get a message (even if the message is "do not update")
-    socket.addEventListener("message", (event) => {
-        setTimeout(update, 100) // give file some time to be written
-        console.log("Message from server:", event.data)
-    })
-} catch (e) {
-    // fall back to polling
-    console.log("Warning: websocket failed " + e + ", falling back to poll")
-    const update2 = () => {
-        update()
-        return setTimeout(update2, 5000)
-    }
-    update2()
-}
-
 map.on('moveend', () => {
     const pos = map.getCenter()
     const z = map.getZoom()
     window.location.hash = `x=${pos.lng}&y=${pos.lat}&z=${z}`
-    setTimeout(x => {
-        const npos = map.getCenter()
-        if ((pos.lng == npos.lng) && (pos.lat == npos.lat)) {
-            console.log("updating")
-            update() // todo: only update if parents have changed
-        }
-    }, 1000)
 })
