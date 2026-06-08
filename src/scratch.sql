7636918a1aa554ed9fde7dbeb40dffcc5101b670
Oliver Blanthorn <freedom4cows@gmail.com>
2026-06-08 16:09:33 +0200

    Sketch random data generator

--- src/scratch.sql
+++ src/scratch.sql
@@ -2,3 +2,6 @@ select index, percent_rank() over (order by value asc) value
 from file('../www/data/h3_data.csv', CSVWithNames, 'index String, value Float64')
 into outfile '../www/data/h3_data.arrow' truncate compression 'none' settings output_format_arrow_compression_method = 'none'
 -- the neat thing here is if we attached it to the server with a bbox we could automatically quantilify whatever was on the screen
+
+-- testing that updates via websockets work (they do)
+select index, randCanonical() value from '2026-03-01.csv' into outfile 'scratch.csv' truncate format CSVWithNames
